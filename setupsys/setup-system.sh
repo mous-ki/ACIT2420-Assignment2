2215ca8952289d068ad91742542e5b412fc06602
miles <146036052+mous-ki@users.noreply.github.com>
2024-11-10 02:02:56 -0800

    Update setup-system.sh

--- setupsys/setup-system.sh
+++ setupsys/setup-system.sh
@@ -8,6 +8,7 @@ usage() {
 	exit 1
 }
 
+
 # Parses options
 while getopts "il" option; do
 	case $option in 
@@ -18,16 +19,16 @@ while getopts "il" option; do
 done
 
 # Runs the install-packges script if -i option is chosen
-if [[ $install_packages -eq 1 ]]; then
+if [ $install_packages -eq 1 ]; then
 	./install-packages.sh
 fi
 
 # Runs the link-configs script if -l option is chosen
-if [[ $link_files -eq 1 ]]; then
+if [ $link_files -eq 1 ]; then
 	./link-configs.sh
 fi
 
 # If no option is chosen, display usage
-if [[ -z $install_packages && -z $link_files ]]; then
+if [ -z $install_packages && -z $link_files ]; then
 	usage
 fi
